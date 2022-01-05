c22221d428818ec8e57242b8182630d5fc0f98e3
Jay <julesmontojo1@gmail.com>
2022-01-05 13:43:22 +0900

    feat: initialize express server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+(async () => {
+  try {
+    app.get('/hello', (req, res) => {
+      res.send('hello world')
+    }),
+
+    app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`)
+    })
+  } catch (err) {
+    console.error("App failed to start", err)
+    process.exit(-1)
+  }
+})();
+

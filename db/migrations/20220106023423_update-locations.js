39919d09ce6e7a5f7baa9b60d3243412e0d47170
Jay <julesmontojo1@gmail.com>
2022-01-06 11:52:06 +0900

    fix: update locations entity in db

--- db/migrations/20220106023423_update-locations.js
+++
@@ -1,8 +0,0 @@
-
-exports.up = function(knex) {
-  return knex.schema.renameTable('location', 'locations')
-};
-
-exports.down = function(knex) {
-  return knex.schema.dropTable('location');
-};
---
+++ db/migrations/20220106024727_create-locations.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('locations', (table) => {
+    table.increments('id')
+    table.string('name').notNullable()
+    table.string('location_address').notNullable()
+    table.float('lat_coordinate').notNullable()
+    table.float('lon_coordinate').notNullable()
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('locations');
+};
